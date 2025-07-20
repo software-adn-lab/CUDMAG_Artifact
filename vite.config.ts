4f9464bb717ad5a4e8e9f318215d3009f1b073b7
Jorge Segovia <jorge.segovia@epn.edu.ec>
2025-07-20 18:44:23 -0500

    chore: configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,20 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// Reemplaza esto con tu nombre exacto de repo
+const repoName = 'Detection-Mapping-of-Antipatterns-DMAP-';
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: `/${repoName}/`,
+
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
