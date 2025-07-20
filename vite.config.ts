9269e945269fff1379be54dca3f692c557f3251b
Jorge Segovia <jorge.segovia@epn.edu.ec>
2025-07-20 18:59:12 -0500

    fix: set correct base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// Reemplaza esto con tu nombre exacto de repo
 const repoName = 'Detection-Mapping-of-Antipatterns-DMAP-';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: `/${repoName}/`,
-
   server: {
     host: "::",
     port: 8080,
